060b9eb4a15800ee1b820175e5f5a62c5f044d13
Logan1xl <mbongowilfried02@gmail.com>
2026-05-20 06:17:02 +0100

    chore: initialisation dépôt et structure projet CRM SavoirManger

---
+++ examen_certifiant_crm_backend/src/main/java/com/examen_certifiant_crm_backend/ExamenCertifiantCrmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.examen_certifiant_crm_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenCertifiantCrmBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenCertifiantCrmBackendApplication.class, args);
+    }
+
+}
